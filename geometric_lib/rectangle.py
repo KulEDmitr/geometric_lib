00ddd7475af16e2d95ae372db9870f32cf6ec791
proolii <144064549+proolii@users.noreply.github.com>
2023-09-05 02:27:42 +0300

    Add files via upload

---
+++ geometric_lib/rectangle.py
@@ -0,0 +1,36 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 CourierNewPS-BoldMT;\f1\fmodern\fcharset0 CourierNewPSMT;\f2\froman\fcharset0 Times-Roman;
+}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red133\green0\blue2;}
+{\*\expandedcolortbl;;\cssrgb\c20000\c20000\c20000;\cssrgb\c60000\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f0\b\fs30\fsmilli15333 \cf2 \expnd0\expndtw0\kerning0
+def
+\f1\b0  
+\f0\b \cf3 area
+\f1\b0 \cf2 (a, b):\'a0
+\f2\fs24 \cf0 \
+
+\f1\fs30\fsmilli15333 \cf2 \'a0\'a0\'a0\'a0
+\f0\b return
+\f1\b0  a * b\'a0
+\f2\fs24 \cf0 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f0\b\fs30\fsmilli15333 \cf2 def
+\f1\b0  
+\f0\b \cf3 perimeter
+\f1\b0 \cf2 (a, b):\'a0
+\f2\fs24 \cf0 \
+
+\f1\fs30\fsmilli15333 \cf2 \'a0\'a0\'a0\'a0
+\f0\b return
+\f1\b0  (a + b) * 2\
+
+\f2\fs24 \cf0 \
+}
---
+++ geometric_lib/triangle.py
@@ -0,0 +1,39 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2709
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 CourierNewPS-BoldMT;\f1\fmodern\fcharset0 CourierNewPSMT;\f2\froman\fcharset0 Times-Roman;
+}
+{\colortbl;\red255\green255\blue255;\red38\green38\blue38;\red133\green0\blue2;\red0\green0\blue0;
+\red14\green110\blue109;}
+{\*\expandedcolortbl;;\cssrgb\c20000\c20000\c20000;\cssrgb\c60000\c0\c0;\cssrgb\c0\c0\c0;
+\cssrgb\c0\c50196\c50196;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f0\b\fs30\fsmilli15333 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 def
+\f1\b0  
+\f0\b \cf3 \strokec3 area
+\f1\b0 \cf2 \strokec2 (a, h):\'a0
+\f2\fs24 \cf0 \strokec4 \
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f1\fs30\fsmilli15333 \cf2 \strokec2 \'a0\'a0\'a0\'a0
+\f0\b return
+\f1\b0  a * h / \cf5 \strokec5 2\cf2 \strokec2 \'a0
+\f2\fs24 \cf0 \strokec4 \
+\pard\pardeftab720\partightenfactor0
+\cf0 \
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f0\b\fs30\fsmilli15333 \cf2 \strokec2 def
+\f1\b0  
+\f0\b \cf3 \strokec3 perimeter
+\f1\b0 \cf2 \strokec2 (a, b, c):\'a0
+\f2\fs24 \cf0 \strokec4 \
+\pard\pardeftab720\ri400\partightenfactor0
+
+\f1\fs30\fsmilli15333 \cf2 \strokec2 \'a0\'a0\'a0\'a0
+\f0\b return
+\f1\b0  a + b + c\'a0
+\f2\fs24 \cf0 \strokec4 \
+}
