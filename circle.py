8b162b1403ec73a7084ce7608ba5381a632967da
ssnchk <143999705+ssnchk@users.noreply.github.com>
2023-09-28 19:42:29 +0300

    circle.py commited

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+''' получаем радиус окружности r и выводим ее площадь r^2*pi'''
     return math.pi * r * r
 
 
 def perimeter(r):
+''' получаем радиус окружности r и выводим ее длину r*2*pi'''
     return 2 * math.pi * r
 
