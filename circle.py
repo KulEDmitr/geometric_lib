ab3f582b9c28a5f08b06ed9d375f268e015baad7
me <me@mail.ru>
2023-10-01 18:26:45 +0300

    README file updated, functions received their comments

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
 
-def area(r):
+def area(r):'''Функция area(r) принимает аргумент r (число) и возвращает площадь окружности с радиусом r'''
     return math.pi * r * r
 
 
-def perimeter(r):
+def perimeter(r):'''Функция perimeter(r) принимает аргумент r (число) и возвращает периметр окружности с радиусом r'''
     return 2 * math.pi * r
 
