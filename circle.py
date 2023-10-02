6ba42c5e4e9021135e1527742520dd1d08e23367
oaoaoammm <olontsevmisha@gmail.com>
2023-10-02 20:22:40 +0300

    docstrings added

--- circle.py
+++ circle.py
@@ -1,10 +1,29 @@
 import math
 
 
-def area(r):
+def area(r: float) -> float:
+    """
+    Возвращает площадь круга радиусом r
+
+	Параметры:
+	    r (float): радиус круга
+
+	Возвращаемое значение:
+	    circle_area (float): Площадь круга заданного радиуса
+    """
     return math.pi * r * r
 
 
-def perimeter(r):
+def perimeter(r: float) -> float:
+    """
+    Возвращает периметр круга радиусом r
+
+    Параметры:
+        r (float): радиус круга
+
+    Возвращаемое значение:
+        circle_perimeter (float): Периметр круга заданного радиуса
+    """
+
     return 2 * math.pi * r
 
