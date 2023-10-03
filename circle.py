56a66a90f890e5e55815c4da41d4fa784c59236d
HumarFlash <68751690+HumarFlash@users.noreply.github.com>
2023-10-04 02:33:27 +0700

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Принимает радиус круга r и возвращает его площадь '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Принимает радиус круга r и возвращает длину окружности '''
     return 2 * math.pi * r
 
