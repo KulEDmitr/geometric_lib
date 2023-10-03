0bfe5d0793b60e7048456ac4cc41444db97420b5
yocho8 <ilya020105@gmail.com>
2023-10-03 22:00:01 +0300

    Changes in README.md

--- circle.py
+++ circle.py
@@ -8,7 +8,7 @@ def area(r):
     '''Функция принимает r <- радиус круга. Возвращает его площадь'''
 
     return math.pi * r * r
-
+    ''' area(3) ; return 9*3,14'''
 
 
 def perimeter(r):
@@ -16,4 +16,4 @@ def perimeter(r):
     '''Функция принимает r <- радиус круга. Возвращает длину его окружности'''
     
     return 2 * math.pi * r
-
+    ''' perimeter(3) ; return 6*3,14 '''
