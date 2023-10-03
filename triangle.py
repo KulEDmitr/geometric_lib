362b03a2ccc3c87afa5e9edb4b35a20ab507795c
HumarFlash <68751690+HumarFlash@users.noreply.github.com>
2023-10-04 03:06:02 +0700

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,22 @@
 def area(a, h):
-    ''' Принимает сторону треугольника a и высоту проведённую к этой стороне h и возвращает его площадь '''
+    ''' Return area of the triangle
+
+            Accepts:
+                a (int): side of the triangle
+                h (int): height of the triangle
+            Return:
+                area (int): area of the triangle
+    '''
     return a * h / 2 
 
 def perimeter(a, b, c):
-    ''' Принимает стороны треугольника a, b, c и возвращает его периметр '''
+    ''' Return perimeter of the triangle
+
+        Accepts:
+            a (int): first side of the triangle
+            b (int): second side of the triangle
+            c (int): third side of the triangle
+        Return:
+            perimeter (int): perimeter of the triangle
+    '''
     return a + b + c 
