fe74de1f04a2fb25a005c3550ae3716c706db731
Настя Полякова <114810379+A-mpol@users.noreply.github.com>
2023-10-04 14:40:16 +0300

    Добавлены комментарии

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''принимает радиус - возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает радиус - возвращает периметр круга'''
     return 2 * math.pi * r
 
