7c9c2dc8ef7d5059f793a26971fafed36c487e18
AlexanderAnynov <sa123122@yandex.ru>
2023-10-04 13:40:40 +0300

    Add documentation

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,11 @@
 def area(a, b):
+    # Takes sides of the rectangle and returns its area
+    # Example:
+    # Print(area(5,5)) // 25
     return a * b
 
 def perimeter(a, b):
+    # Takes sides of the rectangle and returns its perimeter
+    # Example:
+    # Print(area(5,5)) // 20
     return 2a + 2b
--- square.py
+++ square.py
@@ -1,7 +1,13 @@
 
 def area(a):
+    # Takes side of the square and returns its area
+    # Example:
+    # Print(area(5)) // 25
     return a * a
 
 
 def perimeter(a):
+    # Takes side of the square and returns its perimeter
+    # Example:
+    # Print(perimeter(5)) // 20
     return 4 * a
