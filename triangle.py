8914ac23052d0c8bfb4033baaf21c0858ea43a55
Lyapunov Vadim <143802189+kv1sidisi@users.noreply.github.com>
2023-10-05 11:25:26 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,9 +1,11 @@
 def area(a, h):
     ''' Принимает числа a и h, возвращает половину произведения a и h '''
+    '''int(a) = 5, int(h) = 8 -> a * h * 0.5 = 20'''
     return a * h / 2 
-'''int(a) = 5, int(h) = 8 -> a * h * 0.5 = 20'''
+
 
 def perimeter(a, b, c):
     ''' Принимает числа a,b,c, возвращает сумму a,b,c '''
+    '''int(a) = 5, int(b) = 6, int(c) = 3 -> a + b + c = 14'''
     return a + b + c 
-'''int(a) = 5, int(b) = 6, int(c) = 3 -> a + b + c = 14'''
+
