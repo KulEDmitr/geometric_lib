8fd641173bb2587bf6db20ce226610feaf7f3585
Vadim_409040 <lyapunovvadim5002@gmail.com>
2023-10-05 10:31:59 +0300

    Добавлены примеры вызовов функций во всех .py файлах и в документаци.

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,9 @@
-def area(a, b): 
+def area(a, b):
+    ''' Принимает числа a,b, возвращает произведение a и b '''
     return a * b 
+'''int(a) = 5, int(b) = 6 -> a * b = 30'''
 
-def perimeter(a, b): 
+def perimeter(a, b):
+    ''' Принимает числа a,b, возвращает удвоенную сумму чисел a и b '''
     return 2*a + 2*b 
+'''int(a) = 5, int(b) = 6 -> 2 * (a + b) = 22'''
--- triangle.py
+++ triangle.py
@@ -1,6 +1,9 @@
-
-def area(a, h): 
+def area(a, h):
+    ''' Принимает числа a и h, возвращает половину произведения a и h '''
     return a * h / 2 
+'''int(a) = 5, int(h) = 8 -> a * h * 0.5 = 20'''
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    ''' Принимает числа a,b,c, возвращает сумму a,b,c '''
     return a + b + c 
+'''int(a) = 5, int(b) = 6, int(c) = 3 -> a + b + c = 14'''
