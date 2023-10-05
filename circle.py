db595a4568a6de0060c48c2eb759470b1a1433c9
nikita <nkarbofos@gmail.com>
2023-10-05 22:32:17 +0300

    labwork2 done

--- circle.py
+++ circle.py
@@ -2,9 +2,19 @@ import math
 
 
 def area(r):
-    return math.pi * r * r
+	'''Возвращает площадь круга с радиусом r.
+	
+		Параметры:
+			r (int): длина радиуса круга
+		
+		Возвращаемое значение:
+			res (float): площадь круга
+	'''
+	
+	res = math.pi * r * r
+	return res
 
 
 def perimeter(r):
+	''' Принимает число r. Возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
-
