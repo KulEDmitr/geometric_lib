a4ef22fccfce8bb89edf4e501f90c01447f07b83
troxban <pleasebesilent@mail.ru>
2023-10-07 22:17:31 +0300

    documention to circle.py was added

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
-
 def area(r):
+    '''  Принимимает радиус круга, возвращает его площадь   '''
     return math.pi * r * r
 
-
 def perimeter(r):
+    '''  Принимает радиус круга, возвращает его периметр   '''
     return 2 * math.pi * r
 
