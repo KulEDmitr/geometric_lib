aabd1f4503d444edc52608f350feec67ca8ee154
VyacheslavAtamanyuk <vyacheslav.atamanyuk@mail.ru>
2023-10-08 17:52:06 +0300

    Добавлено описание функций для круга

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Принимает число r на вход, возвращает pi * r*r - площадь круга '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r на вход, возвращает 2*pi*r - периметр круга '''
     return 2 * math.pi * r
 
