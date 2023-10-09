9162096d6caaa8b71148bb5f5b37e4ffaf24c905
HumarFlash <68751690+HumarFlash@users.noreply.github.com>
2023-10-09 16:06:16 +0700

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,21 +1,39 @@
 def area(a, b):
-    ''' Return area of the rectangle
+    '''
+    Return area of the rectangle
 
             Accepts:
                 a (int): first side of the rectangle
                 b (int): second side of the rectangle
             Return:
                 area (int): area of the rectangle
+                                
+    Example:
+
+            Accepts:
+                area(3, 4)
+
+            Return:
+                20
     '''
     return a * b 
 
 def perimeter(a, b):
-    ''' Return perimeter of the rectangle
+    '''
+    Return perimeter of the rectangle
 
             Accepts:
                 a (int): first side of the rectangle
                 b (int): second side of the rectangle
             Return:
                 perimeter (int): perimeter of the rectangle
+                                
+    Example:
+
+            Accepts:
+                perimeter(3, 4)
+
+            Return:
+                14
     '''
     return 2*(a + b) 
