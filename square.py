9259613219553335800f43eb1dfbb8986eb45e76
HumarFlash <68751690+HumarFlash@users.noreply.github.com>
2023-10-09 16:08:19 +0700

    Update square.py

--- square.py
+++ square.py
@@ -1,21 +1,39 @@
 
 def area(a):
-    ''' Return area of the square
+    '''
+    Return area of the square
 
             Accepts:
                 a (int): side of the square
             Return:
                 area(int): area of the square
+                                
+    Example:
+
+            Accepts:
+                area(4)
+
+            Return:
+                16
     '''
     return a * a
 
 
 def perimeter(a):
-    ''' Return perimeter of the square
+    '''
+    Return perimeter of the square
 
             Accepts:
                 a (int): side of the square
             Return:
                 perimeter (int): perimeter of the square
+                                
+    Example:
+
+            Accepts:
+                perimeter(3)
+
+            Return:
+                12
     '''
     return 4 * a
