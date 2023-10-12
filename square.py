daadb4d54fb61e974b2bc7e0ec31ca2d093bfd6f
Vladimir Kostyuk <voldemar22864@gmail.com>
2023-10-12 11:03:46 +0300

    fixed: comments and links to commits

--- square.py
+++ square.py
@@ -1,13 +1,17 @@
 
 def area(a): 
-    #Parameter: a (int): integer number
-    #Return value: area of square with side a
-
+    '''
+    Parameter: a (int): integer number
+    Return value: area of square with side a
+    '''
+    
     return a * a
 
 
 def perimeter(a): 
-    #Parameter: a (int): integer number
-    #Return value: area of square with side a
+    '''
+    Parameter: a (int): integer number
+    Return value: area of square with side a
+    '''
 
     return 4 * a
--- triangle.py
+++ triangle.py
@@ -1,14 +1,18 @@
 def area(a, h): 
-    #Parameter: a (int): first integer number
-    #Parameter: h (int): second integer number
-    #Return value: area of triangle with side a and height h
+    '''
+    Parameter: a (int): first integer number
+    Parameter: h (int): second integer number
+    Return value: area of triangle with side a and height h
+    '''
 
     return a * h / 2 
 
 def perimeter(a, b, c): 
-    #Parameter: a (int): first integer number
-    #Parameter: b (int): second integer number
-    #Parameter: c (int): third integer number
-    #Return value: area of square with sides a, b and c
-
+    '''
+    Parameter: a (int): first integer number
+    Parameter: b (int): second integer number
+    Parameter: c (int): third integer number
+    Return value: area of square with sides a, b and c
+    '''
+    
     return a + b + c
