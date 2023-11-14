8b108664256950f3584698a57998bcb17dd73bb7
django228 <d123kbudim@gmail.com>
2023-11-14 21:42:18 +0300

    Revert "unittests!"
    This reverts commit 0496b6a3d422ca24ab115f84b807332a187e4d21.

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,6 @@
 def area(a, b):
     """
     Возвращает произведение двух чисел a и b
-
-    Пример запуска:
-        area(3, 2) --> 6
     """
     return a * b
 
@@ -14,8 +11,5 @@ def perimeter(f, b):
     :param f: одна сторона
     :param b: вторая сторона
     :return: 2*(f + b)
-
-    Пример запуска:
-        perimeter(3, 4) --> 14
     """
     return (f + b) * 2
