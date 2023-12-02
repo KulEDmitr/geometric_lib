6c1e1d556ef25132c1ff11721fc01f8307e7bdd3
Vladimir Kostyuk <voldemar22864@gmail.com>
2023-12-02 12:42:25 +0300

    test commit-2

--- rectangle.py
+++ rectangle.py
@@ -4,7 +4,6 @@ def area(a, b):
     Parameter: b (int): second integer number
     Return value: area of rectangle with sides a and b
     area(3, 4) #returns 12
-    test commit/push, delete this line after test
     '''
 
     return a * b 
