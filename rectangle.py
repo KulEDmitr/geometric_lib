c3bfd4c07aed92b544619e01c7f125c0e52eea01
VyacheslavAtamanyuk <143204016+VyacheslavAtamanyuk@users.noreply.github.com>
2023-12-15 00:44:09 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -37,7 +37,7 @@ class RectangleTestCase(unittest.TestCase):
         self.assertEqual(res, 12)
 
     def test_perimeter_1(self):
-        self.assertEqual(5757, perimeter('5', '7'))
+        self.assertNotEqual(5757, perimeter('5', '7'))
 
     def test_perimeter_2(self):
         self.assertRaises(TypeError, perimeter, 2, '5')
