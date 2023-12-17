27739676fc5424527aff01aec63e3e1b0f787247
Foxman2025 <vladislav.zaxarchenko@mail.ru>
2023-12-17 15:07:01 +0300

    upd

--- circle.py
+++ circle.py
@@ -1,29 +1,4 @@
 import math
-import unittest
-class RectangleTestCase(unittest.TestCase):
-    def test_0_area(self):
-       res = area(0)
-       self.assertEqual(res, 0)
-
-    def test_1_area(self):
-        res = area(100_000)
-        self.assertEqual(res,math.pi*100_000*100_000)
-    @unittest.expectedFailure
-    def test_2_area(self):
-        res = area('a')
-        self.assertEqual(res,TypeError)
-
-    def test_0_perimeter(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
-
-    def test_1_perimeter(self):
-        res = perimeter(100_000)
-        self.assertEqual(res,2*math.pi*100_000)
-    @unittest.expectedFailure
-    def test_2_perimeter(self):
-        res = perimeter('a')
-        self.assertEqual(res,TypeError)
 
 def area(r):
     '''Принимает радиус круга, возвращает площадь круга. Пример вызова функции: area(3) -> 28.274333882308138'''
