c4bdb9d01d5e0d42c6d6c5f3354dec2e399b2bf7
Настя Полякова <114810379+A-mpol@users.noreply.github.com>
2023-12-18 10:48:34 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -16,3 +16,16 @@ def perimeter(r):
     '''
     return 2 * math.pi * r
 
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_mul(self):
+        res = area(10)
+        self.assertEqual(res, 314.1592653589793)
+
+    def test_negative_value(self):
+        res = area(-10)
+        self.assertEqual(res, 314.1592653589793)
