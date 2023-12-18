816b61104a3bb5884c0e6177fab9ae9b7c330139
Настя Полякова <114810379+A-mpol@users.noreply.github.com>
2023-12-18 10:49:48 +0300

    Update square.py

--- square.py
+++ square.py
@@ -13,3 +13,17 @@ def perimeter(a):
     perimeter = 20
     '''
     return 4 * a
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_mul(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_negative_value(self):
+        res = area(-10)
+        self.assertEqual(res, 100)
