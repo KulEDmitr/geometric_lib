7133a04d100b89fbdf734965b16b8c331f0f171f
Артём <76058586+Artem010999@users.noreply.github.com>
2023-12-21 01:49:14 +0300

    Update rectangle_test.py

--- rectangle_test.py
+++ rectangle_test.py
@@ -16,21 +16,6 @@ class RectangleTestCase(unittest.TestCase):
         area_result = area(123456789, 987654321)
         self.assertEqual(area_result, 123456789 * 987654321)
 
-    @unittest.expectedFailure
-    def test_3_area(self):
-        area_result = area("ab", "cd")
-        self.assertEqual(area_result, TypeError)
-
-    @unittest.expectedFailure
-    def test_4_area(self):
-        area_result = area("abcd", 123456789)
-        self.assertEqual(area_result, TypeError)
-
-    def test_5_area(self):
-        area_result = area(-1, 123456789)
-        self.assertEqual(area_result, TypeError)
-
-
 
     def test_0_perimeter(self):
         perimeter_result = perimeter(0, 1)
@@ -44,21 +29,6 @@ class RectangleTestCase(unittest.TestCase):
         perimeter_result = perimeter(123456789, 987654321)
         self.assertEqual(perimeter_result, 2 * (123456789 + 987654321))
 
-    @unittest.expectedFailure
-    def test_3_perimeter(self):
-        perimeter_result = perimeter("abcd", 2)
-        self.assertEqual(perimeter_result, TypeError)
-
-    @unittest.expectedFailure
-    def test_4_perimeter(self):
-        perimeter_result = perimeter("ab", "cd")
-        self.assertEqual(perimeter_result, TypeError)
-
-    def test_5_perimeter(self):
-        perimeter_result = area(-1, 123456789)
-        self.assertEqual(perimeter_result, TypeError)
-
-
 
     def test_0_diagonal(self):
         diagonal_result = diagonal(3, 4)
@@ -71,7 +41,3 @@ class RectangleTestCase(unittest.TestCase):
     def test_2_diagonal(self):
         diagonal_result = diagonal(123456789, 987654321)
         self.assertEqual(diagonal_result, (123456789 ** 2 + 987654321 ** 2) ** 0.5)
-
-    def test_3_diagonal(self):
-        diagonal_result = diagonal(-1, 123456789)
-        self.assertEqual(diagonal_result, TypeError)
