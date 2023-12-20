677b7aa0180ffea86111e8f76420bb6500cec13d
Артём <76058586+Artem010999@users.noreply.github.com>
2023-12-21 01:51:55 +0300

    Update triangle_test.py

--- triangle_test.py
+++ triangle_test.py
@@ -12,22 +12,6 @@ class TriangleTestCase(unittest.TestCase):
 		area_result = area(123456789, 9876543210)
 		self.assertEqual(area_result, (123456789 * 9876543210) / 2)
 
-	@unittest.expectedFailure
-	def test_2_area(self):
-		area_result = area("abcd", 1)
-		self.assertEqual(area_result, TypeError)
-
-	@unittest.expectedFailure
-	def test_3_area(self):
-		area_result = area("ab", "cd")
-		self.assertEqual(area_result, TypeError)
-
-	def test_4_area(self):
-		area_result = area(11, -1)
-		self.assertEqual(area_result, TypeError)
-
-
-
 
 	def test_0_perimeter(self):
 		perimeter_result = perimeter(0, 0, 0)
@@ -36,16 +20,3 @@ class TriangleTestCase(unittest.TestCase):
 	def test_1_perimeter(self):
 		perimeter_result = perimeter(123456789, 9876543210, 1)
 		self.assertEqual(perimeter_result, 123456789 + 9876543210 + 1)
-
-	def test_2_perimeter(self):
-		perimeter_result = perimeter("a", "b", "c")
-		self.assertEqual(perimeter_result, TypeError)
-
-	@unittest.expectedFailure
-	def test_3_perimeter(self):
-		perimeter_result = perimeter("a", 2, "c")
-		self.assertEqual(perimeter_result, TypeError)
-
-	def test_4_perimeter(self):
-		perimeter_result = perimeter(1, -2, 3)
-		self.assertEqual(perimeter_result, TypeError)
