b07aa2b781e84dc6f8953cc7c0033dc10946de00
Артём <76058586+Artem010999@users.noreply.github.com>
2023-12-21 01:50:04 +0300

    Update circle_py.py

--- circle_py.py
+++ circle_py.py
@@ -14,15 +14,6 @@ class CircleTestCase(unittest.TestCase):
         area_result = area(123)
         self.assertEqual(area_result, pi * (123 ** 2))
 
-    @unittest.expectedFailure
-    def test_2_area(self):
-        area_result = area("abcd")
-        self.assertEqual(area_result, TypeError)
-
-    def test_3_area(self):
-        area_result = area(-99)
-        self.assertEqual(area_result, TypeError)
-
 
 
     def test_0_perimeter(self):
@@ -32,13 +23,4 @@ class CircleTestCase(unittest.TestCase):
     def test_1_perimeter(self):
         perimeter_result = perimeter(123)
         self.assertEqual(perimeter_result, 2 * pi * 123)
-
-    @unittest.expectedFailure
-    def test_2_perimeter(self):
-        perimeter_result = perimeter("abcd")
-        self.assertEqual(perimeter_result, TypeError)
-
-    def test_3_perimeter(self):
-        perimeter_result = perimeter(-99)
-        self.assertEqual(perimeter_result, TypeError)
 
