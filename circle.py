8d8aed3b0a5f8d8af629db6cb236565da14ab1ae
Egor Zolotarev <egor.zolotarev2408@mail.ru>
2024-10-15 01:18:23 +0300

    Лаба 2

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    #вычисляет площадь окружности по принимаемому числу r
     return math.pi * r * r
 
 
 def perimeter(r):
+    #вычисляет периметр окружности по принимаемому числу r
     return 2 * math.pi * r
 
