1cac04a7c2c5f7016694f5f759bc1ff49edddd25
George1487 <malygingeorgij17@gmail.com>
2024-10-14 17:21:34 +0300

    Добавили 2 файла в котором в каждом описана работа каждой функции

--- 1.cpp
+++ 1.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 using namespace std;
+int square(int a,int b){
+    //Данная вункция получает на вход 2 параметра : длинну и ширину прямоугольника, на выход она выдает площадь
+    return(a*b);
+}
+int perimetr(int a,int b){
+    //Данная вункция получает на вход 2 параметра : длинну и ширину прямоугольника, на выход она выдает периметр
+    return((a+b)*2);
+}
 int main(){
-    cout<<1234;
+    int a,b,c,d;
+    cin>>a>>b;
+    c = square(a,b);
+    d = perimetr(a,b);
 }
--- 2.cpp
+++ 2.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
+int square(int a,int b){
+    //На вход функция получает 2 параметра: a - основание треугольника , b - высота треугольника. Функция выводит площадь треугольника
+    return (a*b)/2;
+}
+int perimetr(int a,int s1,int s2){
+    //На вход функция получает 3 параметра: a - основание треугольника, s1 и s2 - оставшиеся стороны треугольника. Функция выводит периметр треугольника
+    return (a+s1+s2);
+}
 int main(){
-    int a,b,c=0;
-    cin>>a>>b;
-    c = a*b*0,5;
-    cout<<c;
-    return 0;
+    int a,b,s1,s2,c = 0,d = 0;
+    cin>>a>>b>>s1>>s2;
+    c =  square(a,b);
+    d =  perimetr(a,s1,s2);
 }
