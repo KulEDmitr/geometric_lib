9099c6f613408350477d0fc0f44a70a1d8a98776
Vladimir <donskov.vd@gmail.com>
2024-10-22 23:39:19 +0300

    Добавил в py файлы комментарии, добавил каталог docs

--- circle.py
+++ circle.py
@@ -1,10 +1,21 @@
 import math
 
-
 def area(r):
+    '''
+    Принимает радиус r и возвращает площадь круга
+        Параметры:
+            r (int): Радиус круга
+            math.pi (floar): Число Pi
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Принимает радиус r и возвращает периметр круга
+    Параметры:
+            r (int): Радиус круга
+            math.pi (floar): Число π
+    '''
     return 2 * math.pi * r
 
