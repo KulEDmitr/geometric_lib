e809112016f11390c7a784c96532d11225246e41
Alexander Bogatyrev <91045487+AlexanderPBogatyrev@users.noreply.github.com>
2024-10-23 09:15:18 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,9 @@
 import math
 
-
 def area(r):
+'''Принимает число r, возвращает площадь круга'''
     return math.pi * r * r
 
-
 def perimeter(r):
+'''Принимает число r, возвращает длину окружности'''
     return 2 * math.pi * r
-
