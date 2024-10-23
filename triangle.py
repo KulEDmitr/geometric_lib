889bd7a188a90e11c0a59b83abad6265db855254
Kirill1705 <125290825+Kirill1705@users.noreply.github.com>
2024-10-23 11:16:53 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,13 @@
 def area(a, h): 
+    '''
+    Takes side, height and returns triangle area:
+    :print(area(4, 5)) # Output: 10
+    '''
     return a * h / 2 
 
 def perimeter(a, b, c): 
+    '''
+    Takes sides and returns triangle perimeter:
+    :print(perimeter(4, 5, 3)) # Output: 12
+    '''
     return a + b + c 
