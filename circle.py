3c3cf79eff554dd8e479f69a76726ecdd762d7f6
Kirill1705 <125290825+Kirill1705@users.noreply.github.com>
2024-10-23 11:06:08 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,15 @@ import math
 
 
 def area(r):
+    '''
+    Takes radius of circle and returns its area
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Takes radius of circle and returns its perimeter
+    '''
     return 2 * math.pi * r
 
