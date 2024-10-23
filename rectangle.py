bd0bf16677cc2331d430dcc2ccac4d105bbe7a80
Kirill1705 <125290825+Kirill1705@users.noreply.github.com>
2024-10-23 11:12:24 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,13 @@
 def area(a, b): 
+    '''
+    Takes width, height and returns rectangle area:
+    :print(perimeter(4, 5)) # Output: 20
+    '''
     return a * b 
 
 def perimeter(a, b): 
+    '''
+    Takes width, height and returns rectangle perimeter
+    :print(perimeter(4, 5)) # Output: 18
+    '''
     return a + b 
