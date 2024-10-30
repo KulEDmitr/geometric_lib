d50d34e9fddcefed41cdc09f8ab51658ded1a4c8
GuySky <desyatov2005@gmail.com>
2024-10-31 00:31:41 +0300

    add tests for square.py, triangle.py

--- square.py
+++ square.py
@@ -1,4 +1,7 @@
 '''Возвращает параметры квадрата, принимая на вход одну его сторону'''
+import unittest
+
+
 
 def area(a):
     '''
@@ -22,3 +25,54 @@ def perimeter(a):
     '''
     
     return 4 * a
+
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+       
+    def test_area_one(self):
+        res = area(1)
+        self.assertEqual(res, 1)
+
+    def test_area_positive(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_area_negative(self):
+        res = area(-10)
+        self.assertEqual(res, 100)
+    
+    def test_area_fractional(self):
+        res = area(0.5)
+        self.assertEqual(res, 0.25)
+
+    def test_area_string(self):
+        with self.assertRaises(TypeError):
+            area('abc')
+        
+    
+    def test_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+       
+    def test_perimeter_one(self):
+        res = perimeter(1)
+        self.assertEqual(res, 4)
+
+    def test_perimeter_positive(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+
+    def test_perimeter_negative(self):
+        res = perimeter(-10)
+        self.assertEqual(res, -40)
+    
+    def test_perimeter_fractional(self):
+        res = perimeter(0.5)
+        self.assertEqual(res, 2)
+
+    def test_perimeter_string(self):
+        res = perimeter('abc')
+        self.assertEqual(res, 'abcabcabcabc')
--- triangle.py
+++ triangle.py
@@ -1,4 +1,6 @@
 '''Возвращает параметры треугольника, принимая на вход его стороны и высоту'''
+import unittest
+
 
 def area(a, h):
     '''
@@ -21,3 +23,66 @@ def perimeter(a, b, c):
     '''
     
     return a + b + c
+
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_side_zero(self):
+        res = area(0, 10)
+        self.assertEqual(res, 0)
+
+    def test_area_height_zero(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+       
+    def test_area_side_one(self):
+        res = area(1, 10)
+        self.assertEqual(res, 5)
+
+    def test_area_height_one(self):
+        res = area(10, 1)
+        self.assertEqual(res, 5)
+
+    def test_area_positive(self):
+        res = area(10, 10)
+        self.assertEqual(res, 50)
+
+    def test_area_negative(self):
+        res = area(-10, 10)
+        self.assertEqual(res, -50)
+    
+    def test_area_fractional(self):
+        res = area(10, 0.5)
+        self.assertEqual(res, 2.5)
+
+    def test_area_side_string(self):
+        with self.assertRaises(TypeError):
+            area('abc', 10)
+
+    def test_area_height_string(self):
+        with self.assertRaises(TypeError):
+            area(10, 'abc')
+        
+    
+    def test_perimeter_zero(self):
+        res = perimeter(0, 0, 0)
+        self.assertEqual(res, 0)
+       
+    def test_perimeter_one(self):
+        res = perimeter(1, 1, 1)
+        self.assertEqual(res, 3)
+
+    def test_perimeter_positive(self):
+        res = perimeter(10, 10, 10)
+        self.assertEqual(res, 30)
+
+    def test_perimeter_negative(self):
+        res = perimeter(-10, -10, -10)
+        self.assertEqual(res, -30)
+    
+    def test_perimeter_fractional(self):
+        res = perimeter(0.5, 0.5, 0.5)
+        self.assertEqual(res, 1.5)
+
+    def test_perimeter_string(self):
+        res = perimeter('abc', 'abc', 'abc')
+        self.assertEqual(res, 'abcabcabc')
