01a9df8a2f7f21bf796b269641856ec5775716da
GuySky <desyatov2005@gmail.com>
2024-10-30 23:14:48 +0300

    add tests for rectangle.py, circle.py

--- circle.py
+++ circle.py
@@ -1,5 +1,6 @@
 '''Считает параметры круга, принимая на вход его радиус'''
 import math
+import unittest
 
 
 def area(r):
@@ -23,3 +24,54 @@ def perimeter(r):
 
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area_one(self):
+        res = area(1)
+        self.assertEqual(res, 3.141592653589793)
+
+    def test_area_positive(self):
+        res = area(10)
+        self.assertEqual(res, 314.1592653589793)
+
+    def test_area_negative(self):
+        res = area(-10)
+        self.assertEqual(res, 314.1592653589793)
+    
+    def test_area_fractional(self):
+        res = area(0.5)
+        self.assertEqual(res, 0.7853981633974483)
+    
+    def test_area_string(self):
+        with self.assertRaises(TypeError):
+            area(10, 'abc')
+
+    
+    
+    def test_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+       
+    def test_perimeter_one(self):
+        res = perimeter(1)
+        self.assertEqual(res, 6.283185307179586)
+
+    def test_perimeter_positive(self):
+        res = perimeter(10)
+        self.assertEqual(res, 62.83185307179586)
+
+    def test_perimeter_negative(self):
+        res = perimeter(-10)
+        self.assertEqual(res, -62.83185307179586)
+    
+    def test_perimeter_fractional(self):
+        res = perimeter(0.5)
+        self.assertEqual(res, 3.141592653589793)
+
+    def test_perimeter_string(self):
+        with self.assertRaises(TypeError):
+            perimeter('abc')
--- rectangle.py
+++ rectangle.py
@@ -2,6 +2,7 @@
 Возвращает параметры прямоугольника, принимая
 на вход длины двух его сторон
 '''
+import unittest
 
 def area(a, b):
     '''
@@ -14,6 +15,7 @@ def area(a, b):
     
     return a * b 
 
+
 def perimeter(a, b):
     '''
     Принимает длины двух не противоположных сторон,
@@ -24,3 +26,63 @@ def perimeter(a, b):
     '''
     
     return 2*(a + b)
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+       
+    def test_area_square(self):
+        res = area(10, 10)
+        self.assertEqual(res, 100)
+
+    def test_area_one(self):
+        res = area(10, 1)
+        self.assertEqual(res, 10)
+
+    def test_area_positive(self):
+        res = area(10, 5)
+        self.assertEqual(res, 50)
+
+    def test_area_negative(self):
+        res = area(10, -10)
+        self.assertEqual(res, -100)
+    
+    def test_area_fractional(self):
+        res = area(10, 0.5)
+        self.assertEqual(res, 5)
+
+    def test_area_string(self):
+        res = area(10, 'abc')
+        self.assertEqual(res, 'abcabcabcabcabcabcabcabcabcabc')
+
+        
+    
+    def test_perimeter_zero(self):
+        res = perimeter(10, 0)
+        self.assertEqual(res, 20)
+       
+    def test_perimeter_square(self):
+        res = perimeter(10, 10)
+        self.assertEqual(res, 40)
+
+    def test_perimeter_one(self):
+        res = perimeter(10, 1)
+        self.assertEqual(res, 22)
+
+    def test_perimeter_positive(self):
+        res = perimeter(10, 5)
+        self.assertEqual(res, 30)
+
+    def test_perimeter_negative(self):
+        res = perimeter(10, -10)
+        self.assertEqual(res, 0)
+    
+    def test_perimeter_fractional(self):
+        res = perimeter(10, 0.5)
+        self.assertEqual(res, 21)
+
+    def test_perimeter_string(self):
+        with self.assertRaises(TypeError):
+            perimeter(10, 'abc')
