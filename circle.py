1a5ecc8be2ec4ad5edb9bb5a76af61415e2e42e5
kseni-sch <468150@niuitmo.ru>
2024-11-05 02:10:03 +0300

    Created tests for all functions

--- circle.py
+++ circle.py
@@ -1,24 +1,69 @@
 import math
-
+import unittest
 
 def area(r):
     '''
     Takes circle radius value, returns circle area value
-    
+
     Example:
         area(3)
         28.274333882308138
     '''
+    if not isinstance(r, int):
+        print("Not integer")
+        return 0
+    if r < 0:
+        print("Negative number")
+        return 0
     return math.pi * r * r
 
 
 def perimeter(r):
     '''
     Takes circle radius value, returns circuit value
-    
+
     Example:
         perimeter(3)
         18.84955592153876
     '''
+    if not isinstance(r, int):
+        print("Not integer")
+        return 0
+    if r < 0:
+        print("Negative number")
+        return 0
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_regular_area(self):
+        res = area(10)
+        self.assertEqual(res, 314.1592653589793)
+
+    def test_str_area(self):
+        res = area("a")
+        self.assertEqual(res, 0)
+
+    def test_negative_area(self):
+        res = area(-2)
+        self.assertEqual(res, 0)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_regular_perimeter(self):
+        res = perimeter(3)
+        self.assertEqual(res, 18.84955592153876)
+
+    def test_str_perimeter(self):
+        res = perimeter("a")
+        self.assertEqual(res, 0)
+
+    def test_negative_perimeter(self):
+        res = perimeter(-2)
+        self.assertEqual(res, 0)
--- triangle.py
+++ triangle.py
@@ -1,3 +1,4 @@
+import unittest
 
 def area(a, h):
     '''
@@ -7,6 +8,12 @@ def area(a, h):
         area(3, 4)
         6
     '''
+    if not isinstance(a, int) or not isinstance(h, int):
+        print("Not integer")
+        return 0
+    if a < 0 or h < 0:
+        print("Negative number")
+        return 0
     return a * h / 2
 
 def perimeter(a, b, c):
@@ -17,4 +24,44 @@ def perimeter(a, b, c):
         perimeter(3, 2, 5)
         10
     '''
+    if not isinstance(a, int) or not isinstance(b, int) or not isinstance(c, int):
+        print("Not integer")
+        return 0
+    if a < 0 or b < 0 or c < 0:
+        print("Negative number")
+        return 0
     return a + b + c
+
+
+class TriangleTestCase(unittest.TestCase):
+	def test_zero_area(self):
+		res = area(10, 0)
+		self.assertEqual(res, 0)
+
+	def test_regular_area(self):
+		res = area(5, 3)
+		self.assertEqual(res, 7.5)
+
+	def test_str_area(self):
+		res = area(10, "a")
+		self.assertEqual(res, 0)
+
+	def test_negative_area(self):
+		res = area(10, -2)
+		self.assertEqual(res, 0)
+
+	def test_zero_perimeter(self):
+		res = perimeter(10, 0, 4)
+		self.assertEqual(res, 14)
+
+	def test_regular_perimeter(self):
+		res = perimeter(10, 5, 7)
+		self.assertEqual(res, 22)
+
+	def test_str_perimeter(self):
+		res = perimeter(10, "a", 3)
+		self.assertEqual(res, 0)
+
+	def test_negative_perimeter(self):
+		res = perimeter(10, -2, 5)
+		self.assertEqual(res, 0)
