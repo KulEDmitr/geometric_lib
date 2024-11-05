3bc8e847540ecf57c74fe4072f4675da268d1bb2
HuntedDuck <159060632+HuntedDuck@users.noreply.github.com>
2024-11-05 13:00:37 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,31 @@
 import math
 
 
+
+
 def area(r):
+    '''
+Returns the area of circle with given radius
+
+Parameters: 
+    int r: radius of the circle
+
+Return value:
+    (int): area of the circle with given radius
+'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+Returns the perimeter of circle with given radius
+
+Parameters: 
+    int r: radius of the circle
+
+Return value:
+    (int): perimeter of the circle with given radius
+'''
+
     return 2 * math.pi * r
 
