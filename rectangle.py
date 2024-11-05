99254da8956423a955ff6f6f356d1ea013ff0af5
HuntedDuck <159060632+HuntedDuck@users.noreply.github.com>
2024-11-05 13:03:33 +0300

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,25 @@
 def area(a, b):
+    '''
+Area of rectangle with given sides
+
+Parameters: 
+    int a,b: sides of the rectangle
+
+Return value:
+    (int): area of the rectangle with given sides
+'''
+
     return a * b
 
 def perimeter(a, b):
+    '''
+Returns the perimeter of rectangle with given sides
+
+Parameters: 
+    int a,b: sides of the rectangle
+
+Return value:
+    (int): perimeter of rectangle with given sides
+'''
+
     return (a + b)*2 
