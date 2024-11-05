0a83ed05072ecb988dbf9774290708a82b9c9d64
HuntedDuck <159060632+HuntedDuck@users.noreply.github.com>
2024-11-05 13:07:35 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,23 @@
 def area(a, b):
+    '''
+Area of triangle with given sides
+
+Parameters: 
+    int a,b: sides of the triangle
+
+Return value:
+    (int): area of the triangle with given sides
+'''
     return a * b
 
 def perimeter(a, b):
+    '''
+Area of triangle with given sides
+
+Parameters: 
+    int a,b: sides of the triangle
+
+Return value:
+    (int): area of the triangle with given sides
+'''
     return a + b
