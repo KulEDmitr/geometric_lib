af6bceeba7f048753548ada2450e80734da2fad9
Egor Zolotarev <76940114+egorzolotarev@users.noreply.github.com>
2024-11-13 11:12:10 +0300

    Update UnitTestSquare.py

--- UnitTestSquare.py
+++ UnitTestSquare.py
@@ -2,7 +2,7 @@ import unittest
 from square import *
 
 '''
-Данные тесты проверяют работу функций файла square.py, проверяя его функции araa и perimetr по 2 раза
+Данные тесты проверяют работу функций файла square.py, проверяя его функции araa и perimetr по 4 раза
 '''
 class SquareTestCase(unittest.TestCase):
     def test_zero_area(self):
@@ -13,6 +13,14 @@ class SquareTestCase(unittest.TestCase):
         res = area(10000)
         self.assertEqual(res, 100000000)
 
+    def test_float_area(self):
+        res = area(0.5)
+        self.assertEqual(res, 0.25)
+
+    def test_string_area(self):
+        res = area('10000')
+        self.assertEqual(res, 100000000)
+
     def test_nul_per(self):
         res = perimeter(0)
         self.assertEqual(res, 0)
@@ -20,3 +28,13 @@ class SquareTestCase(unittest.TestCase):
     def test_big_per(self):
         res = perimeter(10000)
         self.assertEqual(res, 40000)
+
+    def test_float_per(self):
+        res = perimeter(0.5)
+        self.assertEqual(res, 2)
+
+    def test_string_per(self):
+        res = perimeter('10000')
+        self.assertEqual(res, 40000)
+
+
