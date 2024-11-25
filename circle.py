3a4a4ce62549f7b70188a25bec0cb73a0dd4dfba
Konstantin Nebykov <ptichka816@mail.ru>
2024-11-25 19:24:38 +0300

    Добавлены unit-тесты для существующих функций

--- circle.py
+++ circle.py
@@ -2,8 +2,16 @@ import math
 
 
 def area(r):
+    if not isinstance(r, (int, float)):
+        raise TypeError("Value must be number")
+    if r < 0:
+        raise ValueError("Value must be positive")
     return math.pi * r * r
 
 
 def perimeter(r):
+    if not isinstance(r, (int, float)):
+        raise TypeError("Value must be number")
+    if r < 0:
+        raise ValueError("Value must be positive")
     return 2 * math.pi * r
--- triangle.py
+++ triangle.py
@@ -1,6 +1,14 @@
 def area(a, h):
+    if not isinstance(a, (int, float)) or not isinstance(h, (int, float)):
+        raise TypeError("Values must be numbers")
+    if a < 0 or h < 0:
+        raise ValueError("Values must be positive")
     return a * h / 2
 
 
 def perimeter(a, b, c):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float) or not isinstance(c, (int, float))):
+        raise TypeError("Values must be numbers")
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("Values must be positive")
     return a + b + c
