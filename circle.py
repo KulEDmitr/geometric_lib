c7aac9ed988b3d26e693abe719a2db2a281e5d9d
tiltdepressed <vladisaev600@mail.ru>
2024-11-28 21:43:47 +0300

    new files, readme and tests

--- circle.py
+++ circle.py
@@ -1,10 +1,34 @@
 import math
+class Circle:
+    def area(r):
+        ''' 
+        Вычисляет площадь оркужности радиуса r
 
+            Входные данные:
+                double r : радиус окружности
 
-def area(r):
-    return math.pi * r * r
+            Выходные данные:
+                double area : площадь оружности
+        
+        Пример работы:
+            >> area(5)
+            >> 78,539816338
+        '''
+        return math.pi * r * r
 
+    def perimeter(r):
+        '''
+        Вычисляет длину окружности 
+        радиуса r
 
-def perimeter(r):
-    return 2 * math.pi * r
-
+            Входные данные:
+                double r : радиус окружности
+            
+            Выходные данные:
+                double perimeter : длина окружности радиуса r
+            
+        Пример работы:
+            >> perimeter(5)
+            >> 31,415926535
+        '''
+        return 2 * math.pi * r
