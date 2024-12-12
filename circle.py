b8d98a82dcb1e6bca25770def8c32bfb5c2284c4
Artemii <arttomcer@gmail.com>
2024-12-12 15:32:04 +0300

    добавление комментариев

--- circle.py
+++ circle.py
@@ -2,9 +2,26 @@ import math
 
 
 def area(r):
+    """
+    На вход поступает число r, возвращает r^2*pi
+    Пример:
+        Ввод:
+        r=2
+        Вывод:
+        area=12.56
+    """
     return math.pi * r * r
+    
 
 
 def perimeter(r):
+    """
+    На вход поступает число r, возвращает r*2*pi
+    Пример:
+        Ввод:
+        r=4
+        Вывод:
+        perimeter=25.132
+    """
     return 2 * math.pi * r
 
