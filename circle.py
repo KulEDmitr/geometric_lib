a777b8362a90344ffe134b0325476b4ed778f4d0
tokyoboy0701x <sotnikovivan3000@yandex.ru>
2024-12-25 02:28:12 +0300

    unittests added

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+from unittest import TestCase
 
 def area(r):
     '''Возвращает площадь круга с радиусом r.
@@ -10,6 +10,13 @@ def area(r):
             Пример вызова area(3):
                     Входные данные: 3
                     Вывод: 28.2743338823'''
+
+    if not (isinstance(r, int) or isinstance(r, float)):
+        raise TypeError("Argument must be int or float")
+
+    if r <= 0:
+        raise ValueError("Argument must be positive")
+
     return math.pi * r * r
 
 
@@ -22,5 +29,45 @@ def perimeter(r):
             Пример вызова perimetr(3):
                     Входные данные: 3
                     Вывод: 18.8495559215'''
+
+    if not (isinstance(r, int) or isinstance(r, float)):
+        raise TypeError("Argument must be int or float")
+
+    if r <= 0:
+        raise ValueError("Argument must be positive")
+
     return 2 * math.pi * r
 
+
+
+
+class CircleTestCase(TestCase):
+    def test_area_with_integer_radius(self):
+        self.assertEqual(area(5), 78.53981633974483)
+
+    def test_area_with_float_radius(self):
+        self.assertEqual(area(2.5), 19.634954084936208)
+
+    def test_area_with_incorrect_type_radius(self):
+        self.assertRaises(TypeError, area, [])
+
+    def test_area_with_negative_radius(self):
+        self.assertRaises(ValueError, area, -1)
+
+    def test_area_with_zero_radius(self):
+        self.assertRaises(ValueError, area, 0)
+
+    def test_perimeter_with_integer_radius(self):
+        self.assertEqual(perimeter(4), 25.132741228718345)
+
+    def test_perimeter_with_float_radius(self):
+        self.assertEqual(perimeter(3.5), 21.991148575128552)
+
+    def test_perimeter_with_incorrect_type_radius(self):
+        self.assertRaises(TypeError, perimeter, [])
+
+    def test_perimeter_with_negative_radius(self):
+        self.assertRaises(ValueError, perimeter, -2)
+
+    def test_perimeter_with_zero_radius(self):
+        self.assertRaises(ValueError, perimeter, 0)
--- square.py
+++ square.py
@@ -1,3 +1,4 @@
+from unittest import TestCase
 
 def area(a):
     '''Возвращает площадь квадрата со стороной a
@@ -8,6 +9,13 @@ def area(a):
         Пример вызова area(3):
             Входные данные: 3
             Вывод: 9'''
+
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("Argument must be int or float")
+
+    if a <= 0:
+        raise ValueError("Argument must be positive")
+
     return a * a
 
 
@@ -20,4 +28,42 @@ def perimeter(a):
         Пример вызова perimetr(3):
             Входные данные: 3
             Вывод: 12'''
+
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("Argument must be int or float")
+
+    if a <= 0:
+        raise ValueError("Argument must be positive")
+
     return 4 * a
+
+class SquareTestCase(TestCase):
+    def test_area_with_integer_dimension(self):
+        self.assertEqual(area(3), 9)
+
+    def test_area_with_float_dimension(self):
+        self.assertEqual(area(2.5), 6.25)
+
+    def test_area_with_incorrect_type(self):
+        self.assertRaises(TypeError, area, [])
+
+    def test_area_with_negative_value(self):
+        self.assertRaises(ValueError, area, -1)
+
+    def test_area_with_zero_value(self):
+        self.assertRaises(ValueError, area, 0)
+
+    def test_perimeter_with_integer_dimension(self):
+        self.assertEqual(perimeter(3), 12)
+
+    def test_perimeter_with_float_dimension(self):
+        self.assertEqual(perimeter(2.5), 10.0)
+
+    def test_perimeter_with_incorrect_type(self):
+        self.assertRaises(TypeError, perimeter, [])
+
+    def test_perimeter_with_negative_value(self):
+        self.assertRaises(ValueError, perimeter, -1)
+
+    def test_perimeter_with_zero_value(self):
+        self.assertRaises(ValueError, perimeter, 0)
